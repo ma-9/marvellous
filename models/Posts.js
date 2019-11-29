eb8a973149f3e9c7bec04004b11b733751e383fe
Manav Oza <manavoza99@gmail.com>
2019-11-30 00:01:40 +0530

    Model for Posts INstallled

---
+++ models/Posts.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  text: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  name: {
+    type: mongoose.Schema.Types.String
+  },
+  avatar: {
+    type: mongoose.Schema.Types.String
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+      },
+      text: {
+        type: mongoose.Schema.Types.String,
+        required: true
+      },
+      name: {
+        type: mongoose.Schema.Types.String
+      },
+      avatar: {
+        type: mongoose.Schema.Types.String
+      },
+      date: {
+        type: mongoose.Schema.Types.Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: mongoose.Schema.Types.Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('post', PostSchema);
