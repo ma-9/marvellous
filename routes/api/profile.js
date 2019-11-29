ce6c0a96ef7194e07902ae47ac9f68448ab77dfd
Manav Oza <manavoza99@gmail.com>
2019-11-29 21:45:22 +0530

    Delete Profile Experience Installed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -205,4 +205,27 @@ router.put('/', auth, async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile/:exp_id
+// @desc    Delete Profile Experience Using Exp ID
+// @access  Public
+router.delete('/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({
+      msg: 'Server Error'
+    });
+  }
+});
+
 module.exports = router;
