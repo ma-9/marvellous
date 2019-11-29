5d0c7fbcffbd1207f3d4753182863547364ef0dd
Manav Oza <manavoza99@gmail.com>
2019-11-29 12:36:27 +0530

    Login to get Token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const auth = require('../../middlewares/auth');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 // @route   GET api/auth
-// @desc    test auth
-// @access  public
+// @desc    Authenticate User via Token and Show User Details as response
+// @access  private
 router.get('/', auth, async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
@@ -14,5 +18,69 @@ router.get('/', auth, async (req, res, next) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/auth
+// @desc    Logging in to get Token
+// @access  public
+router.post(
+  '/',
+  [
+    check('email', 'Invalid Email').isEmail(),
+    check('password', 'Password is Required').exists()
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: errors.array()
+      });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if User Exists
+      let user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(400).json([
+          {
+            msg: 'Invalid Credentials'
+          }
+        ]);
+      }
+
+      const isMatched = await bcrypt.compare(password, user.password);
+
+      if (!isMatched) {
+        return res.status(400).json([
+          {
+            msg: 'Invalid Credentials'
+          }
+        ]);
+      }
+
+      // Return JSON Web Token
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -34,9 +34,11 @@ router.post(
       let user = await User.findOne({ email: email });
 
       if (user) {
-        return res.status(400).json({
-          msg: 'User Already Exists'
-        });
+        return res.status(400).json([
+          {
+            msg: 'User Already Exists'
+          }
+        ]);
       }
 
       // Get Users Gravatar
