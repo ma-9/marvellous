f0e69c6bc357296975877c1c19527b8835e941fa
Manav Oza <manavoza99@gmail.com>
2019-11-29 16:00:25 +0530

    Individual profile Route Installed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,30 @@
 const router = require('express').Router();
+const auth = require('../../middlewares/auth');
+const Profile = require('../../models/Profile');
 
-// @route   GET api/profile
-// @desc    test profile
-// @access  public
-router.get('/', (req, res, next) => {
-  res.send('profile Route');
+// @route   GET api/profile/me
+// @desc    Get Individual Profile Details
+// @access  Private
+router.get('/me', auth, async (req, res, next) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({
+        msg: 'There is no Profile For This User'
+      });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      msg: 'Server Error',
+      error: error
+    });
+  }
 });
 
 module.exports = router;
