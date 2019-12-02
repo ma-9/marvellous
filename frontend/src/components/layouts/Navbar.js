dd27ce017c8cb7fb2b57f05ec8e24d06bbabbce4
Manav Oza <manavoza99@gmail.com>
2019-12-02 19:19:01 +0530

    Sign Up Form ADD

--- frontend/src/components/layouts/Navbar.js
+++ frontend/src/components/layouts/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
       </h1>
       <ul>
         <li>
-          <Link to='!#'>Developers</Link>
+          <Link to='/'>Developers</Link>
         </li>
         <li>
           <Link to='/register'>Register</Link>
