2c0ac87c1d41c9b77d6da3c66699bc8a1498ad39
Manav Oza <manavoza99@gmail.com>
2019-12-07 19:55:55 +0530

    Changed Primary color to #240b36

--- frontend/src/components/layouts/Navbar.js
+++ frontend/src/components/layouts/Navbar.js
@@ -12,11 +12,10 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    backgroundColor: '#240b36'
   },
   menuButton: {
     marginRight: theme.spacing(2)
@@ -65,8 +64,8 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   );
 
   return (
-    <div className={classes.root}>
-      <AppBar position='fixed'>
+    <div>
+      <AppBar position='fixed' className={classes.root}>
         <Toolbar>
           <IconButton
             edge='start'
@@ -100,69 +99,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, { logout })(Navbar);
-
-// import React, { Fragment } from 'react';
-// import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
-// import { logout } from '../../actions/auth';
-// import PersonIcon from '@material-ui/icons/Person';
-// import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-// import DeveloperModeIcon from '@material-ui/icons/DeveloperMode';
-
-// const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
-//   const authLinks = (
-//     <ul>
-//       <li>
-//         <Link to='/dashboard' style={{ display: 'flex' }}>
-//           <PersonIcon />
-//           <span className='hide-sm'>Dashboard</span>
-//         </Link>
-//       </li>
-//       <li>
-//         <a onClick={logout} href='#!' style={{ display: 'flex' }}>
-//           <ExitToAppIcon />
-//           <span className='hide-sm'>Logout</span>
-//         </a>
-//       </li>
-//     </ul>
-//   );
-
-//   const guestLinks = (
-//     <ul>
-//       <li>
-//         <Link to='/'>Developers</Link>
-//       </li>
-//       <li>
-//         <Link to='/register'>Register</Link>
-//       </li>
-//       <li>
-//         <Link to='/login'>Login</Link>
-//       </li>
-//     </ul>
-//   );
-
-//   return (
-//     <li className='navbar bg-dark'>
-//       <h1>
-//         <Link to='/'>
-//           <DeveloperModeIcon /> Marvellous IN
-//         </Link>
-//       </h1>
-// {!loading && (
-//   <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-// )}
-//     </li>
-//   );
-// };
-
-// Navbar.propTypes = {
-//   logout: PropTypes.func.isRequired,
-//   auth: PropTypes.object.isRequired
-// };
-
-// const mapStateToProps = (state) => ({
-//   auth: state.auth
-// });
-
-// export default connect(mapStateToProps, { logout })(Navbar);
