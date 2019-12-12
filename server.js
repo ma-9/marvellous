e90cd70526e4d0c8c0acaf06830f3fa9ee8f1418
Manav Oza <manavoza99@gmail.com>
2019-12-13 00:50:16 +0530

    changed Static build folder

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 // Serve Static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set Static Folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
