4c798e8c85a4d3b4228998ab2047bf09c4551de8
Manav Oza <manavoza99@gmail.com>
2019-12-12 23:27:13 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/dbConnect');
 const cors = require('cors');
+const path = require('path');
 
 const port = process.env.PORT || 5000;
 
@@ -14,9 +15,6 @@ app.use(
   })
 );
 
-app.get('/', (req, res, next) => {
-  res.send('API Running');
-});
 // Database Connection
 connectDB();
 
@@ -25,6 +23,18 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('../frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    );
+  });
+}
 
 app.listen(port, (err) => {
   if (!err) {
