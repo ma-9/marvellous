322be634eb98dba0039c4b6f559a26bcc0cb5147
Manav Oza <manavoza99@gmail.com>
2020-01-11 19:52:53 +0530

    React Animation Added

--- frontend/src/components/layouts/LandingPage/index.js
+++ frontend/src/components/layouts/LandingPage/index.js
@@ -7,6 +7,9 @@ import useStyles from './style';
 import WelcomeScreen from '../../../img/coder.gif';
 import { fadeIn } from 'react-animations';
 import Radium, { StyleRoot } from 'radium';
+import ContactIcon from '@material-ui/icons/RecentActors';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import BookIcon from '@material-ui/icons/Book';
 const AnimatedStyles = {
   animate: {
     animation: 'x 5s',
@@ -35,23 +38,30 @@ const Landing = ({ isAuthenticated }) => {
                 <p className={classes.tagLine}>
                   A Blogging Platform for Developers
                 </p>
+                <br />
+                <p className={classes.paraGraph}>
+                  We are very delighted to welcome you as a new member of our
+                  firm. We strongly believe that your skill and knowledge will
+                  help us achieve what we aim for!
+                </p>
               </div>
             </StyleRoot>
             <br />
+            <br />
             <StyleRoot>
               <div style={AnimatedStyles.animate}>
-                <Grid container>
-                  <Grid item xs={6} sm={6}>
-                    <Link to='/register'>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <Link to='/register' className='center'>
                       <Button variant='contained' color='primary'>
-                        Register
+                        Sign up with us Today !
                       </Button>
                     </Link>
                   </Grid>
-                  <Grid item xs={6} sm={6}>
-                    <Link to='/login'>
+                  <Grid item xs={12} sm={6}>
+                    <Link to='/login' className='center'>
                       <Button variant='contained' color='secondary'>
-                        Login
+                        Login, Welcome Back
                       </Button>
                     </Link>
                   </Grid>
@@ -69,6 +79,66 @@ const Landing = ({ isAuthenticated }) => {
                 alt='Welcome to Marvellous'
               />
             </StyleRoot>
+          </div>
+        </Grid>
+      </Grid>
+      <br />
+      <br />
+      <br />
+      <Grid container spacing={3}>
+        <StyleRoot>
+          <Grid item xs={12} sm={12}>
+            <div className={classes.centerHeading}>
+              <p style={AnimatedStyles.animate}>Our Mission</p>
+            </div>
+            <div className={classes.missionContent}>
+              <p style={AnimatedStyles.animate}>
+                “To collect, preserve, study, exhibit, and stimulate
+                appreciation for and advance knowledge of works of art that
+                collectively represent the broadest spectrum of human
+                achievement at the highest level of quality, all in the service
+                of the public and in accordance with the highest professional
+                standards.”
+              </p>
+              <br />
+              <br />
+            </div>
+          </Grid>
+        </StyleRoot>
+
+        <Grid item xs={12} sm={4}>
+          <div className={classes.ourMissionTabs}>
+            <div className='center'>
+              <ContactIcon style={{ color: '#240b36', fontSize: '3rem' }} />
+            </div>
+            <br />
+            <p className='center'>
+              "Thousands of Successful story beign with us!"
+            </p>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div className={classes.ourMissionTabs}>
+            <div className='center'>
+              <GitHubIcon style={{ color: '#240b36', fontSize: '3rem' }} />
+            </div>
+            <br />
+            <p className='center'>
+              "Don't make noise with your energy, make larger noise with your
+              code"
+            </p>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div className={classes.ourMissionTabs}>
+            <div className='center'>
+              <BookIcon style={{ color: '#240b36', fontSize: '3rem' }} />
+            </div>
+            <br />
+            <p className='center'>
+              "Contribute to Open-Source and Help Developers via Posting and
+              Replying to them"
+            </p>
           </div>
         </Grid>
       </Grid>
--- frontend/src/components/layouts/LandingPage/style.js
+++ frontend/src/components/layouts/LandingPage/style.js
@@ -2,7 +2,7 @@ import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
   title: {
-    color: 'red',
+    color: '#240b36',
     fontFamily: 'Muli',
     fontSize: '3rem',
     letterSpacing: 2
@@ -11,6 +11,34 @@ const useStyles = createUseStyles({
     color: '#333',
     fontSize: '1rem',
     letterSpacing: 4
+  },
+  paraGraph: {
+    fontSize: '1.4rem',
+    fontFamily: 'Muli',
+    wordSpacing: 2
+  },
+  centerHeading: {
+    fontSize: '3rem',
+    fontFamily: 'Muli',
+    textAlign: 'center',
+    letterSpacing: 2,
+    color: '#240b36'
+  },
+  missionContent: {
+    fontFamily: 'Muli',
+    textAlign: 'center',
+    wordSpacing: 5,
+    fontSize: '1rem'
+  },
+  ourMissionTabs: {
+    backgroundColor: '#D7D3D7',
+    padding: '1rem',
+    height: '8rem',
+    fontWeight: 'bold',
+    '&:hover': {
+      backgroundColor: '#F99A9A',
+      color: '#fff'
+    }
   }
 });
 
