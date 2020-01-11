f2cc2d95906e68f5b063687edb8a7cf5e4aabd58
Manav Oza <manavoza99@gmail.com>
2020-01-11 14:52:17 +0530

    AppDrawer Updated

--- frontend/src/components/layouts/Appbar.js
+++ frontend/src/components/layouts/Appbar.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Link,  Route, Switch } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
+
+import Techies from '@material-ui/icons/People';
 
 import Landing from './Landing';
 import Routes from '../routing/Routes';
@@ -24,73 +26,81 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import LogoutIcon from '@material-ui/icons/ExitToApp';
+import LoginIcon from '@material-ui/icons/VpnKey';
+import RegisterIcon from '@material-ui/icons/GroupAdd';
+import PersonIcon from '@material-ui/icons/Person';
+import PostIcon from '@material-ui/icons/Book';
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: 'flex'
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: '#240b36',
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 36
   },
   hide: {
-    display: 'none',
+    display: 'none'
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: 'nowrap'
   },
   drawerOpen: {
     width: drawerWidth,
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   drawerClose: {
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
     overflowX: 'hidden',
     width: theme.spacing(7) + 1,
     [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
+      width: theme.spacing(9) + 1
+    }
   },
   toolbar: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-end',
     padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
+    ...theme.mixins.toolbar
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
-  },
+    padding: theme.spacing(3)
+  }
 }));
 
-export default function MiniDrawer() {
+const AppDrawer = (props) => {
+  const {
+    auth: { isAuthenticated, loading },
+    logout
+  } = props;
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -103,75 +113,136 @@ export default function MiniDrawer() {
     setOpen(false);
   };
 
+  const guestLinks = (
+    <List>
+      <Link to='/register'>
+        <ListItem button>
+          <ListItemIcon>
+            <RegisterIcon style={{ color: '#240b36' }} />
+          </ListItemIcon>
+          <ListItemText primary='Register' />
+        </ListItem>
+      </Link>
+      <Link to='/login'>
+        <ListItem button>
+          <ListItemIcon>
+            <LoginIcon style={{ color: '#240b36' }} />
+          </ListItemIcon>
+          <ListItemText primary='Login' />
+        </ListItem>
+      </Link>
+    </List>
+  );
+
+  const authLinks = (
+    <List>
+      <Link to='/posts'>
+        <ListItem button>
+          <ListItemIcon>
+            <PostIcon style={{ color: '#240b36' }} />
+          </ListItemIcon>
+          <ListItemText primary='Posts' />
+        </ListItem>
+      </Link>
+      <Link to='/dashboard'>
+        <ListItem button>
+          <ListItemIcon>
+            <PersonIcon style={{ color: '#240b36' }} />
+          </ListItemIcon>
+          <ListItemText primary='Dashboard' />
+        </ListItem>
+      </Link>
+      <a onClick={logout} href='#!' rel='noopener noreferrer'>
+        <ListItem button>
+          <ListItemIcon>
+            <LogoutIcon style={{ color: '#240b36' }} />
+          </ListItemIcon>
+          <ListItemText primary='Logout' />
+        </ListItem>
+      </a>
+    </List>
+  );
+
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
-        position="fixed"
+        position='fixed'
         className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
+          [classes.appBarShift]: open
         })}
       >
         <Toolbar>
           <IconButton
-            color="inherit"
-            aria-label="open drawer"
+            color='inherit'
+            aria-label='open drawer'
             onClick={handleDrawerOpen}
-            edge="start"
+            edge='start'
             className={clsx(classes.menuButton, {
-              [classes.hide]: open,
+              [classes.hide]: open
             })}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap>
-            Mini variant drawer
+          <Typography variant='h6' noWrap>
+            Marvellous In
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
-        variant="permanent"
+        variant='permanent'
         className={clsx(classes.drawer, {
           [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
+          [classes.drawerClose]: !open
         })}
         classes={{
           paper: clsx({
             [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
+            [classes.drawerClose]: !open
+          })
         }}
       >
         <div className={classes.toolbar}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+          <Link to='/profiles'>
+            <ListItem button>
+              <ListItemIcon>
+                <Techies style={{ color: '#240b36' }} />
+              </ListItemIcon>
+              <ListItemText primary='Techies' />
             </ListItem>
-          ))}
+          </Link>
         </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        {!loading && (
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        )}
       </Drawer>
-      <main className={classes.content}>
-      <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route component={Routes} />
-          </Switch>
+      <main>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route component={Routes} />
+        </Switch>
       </main>
     </div>
   );
-}
+};
+
+AppDrawer.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(AppDrawer);
